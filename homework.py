02aeb3b3a06674be132f80e1214145589ed8c3db
AradhyaAryal <aryalaradhya3@gmail.com>
2026-06-07 15:58:51 -0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+# Store birthday dates in 5 different variables
+birthday1 = date(1990, 5, 15)
+birthday2 = date(1985, 10, 23)
+birthday3 = date(2000, 1, 1)
+birthday4 = date(1992, 7, 30)
+birthday5 = date(1978, 12, 5)
+
+print(f"Birthday 1: {birthday1}")
+print(f"Birthday 2: {birthday2}")
+print(f"Birthday 3: {birthday3}")
+print(f"Birthday 4: {birthday4}")
+print(f"Birthday 5: {birthday5}")
